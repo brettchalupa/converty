3742c8eaff8a7b3d7492bec8cef6aad5be8a62a4
Brett Chalupa <brett@bigcartel.com>
2021-04-26 17:26:22 -0400

    Add initial Converty.convert method for miles & kilometers
    This allows the conversion to happen between the two units as a way to
    get the code built out and working.

--- lib/converty.rb
+++ lib/converty.rb
@@ -2,7 +2,37 @@
 
 require_relative "converty/version"
 
+# Easily convert amounts between different units
 module Converty
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CantConvertError < Error; end
+
+  KILOMETERS_PER_MILE = 1.609344
+
+  # Convert the specified amount in the +from+ unit to the to the +to+ unit
+  #
+  # It does not handle rounding the returned conversion.
+  #
+  # Examples:
+  #
+  #  Converty.convert(5, from: :km, to: :mi).round(1) => 3.1
+  def self.convert(amount, from:, to:)
+    case from
+    when :mi
+      case to
+      when :km
+        amount * KILOMETERS_PER_MILE
+      else
+        raise CantConvertError
+      end
+    when :km
+      case to
+      when :mi
+        amount / KILOMETERS_PER_MILE
+      else
+        raise CantConvertError
+      end
+    end
+  end
 end
--- spec/converty_spec.rb
+++ spec/converty_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe Converty do
     expect(Converty::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".convert" do
+    it "supports miles to kilometers" do
+      expect(Converty.convert(3.1, from: :mi, to: :km).round(1)).to eql(5.0)
+    end
+
+    it "supports kilometers to miles" do
+      expect(Converty.convert(5, from: :km, to: :mi).round(1)).to eql(3.1)
+    end
+
+    it "throws an error when the units aren't compatible" do
+      expect { Converty.convert(5, from: :mi, to: :g) }.to raise_error(Converty::CantConvertError)
+
+      expect { Converty.convert(5, from: :km, to: :g) }.to raise_error(Converty::CantConvertError)
+    end
   end
 end
