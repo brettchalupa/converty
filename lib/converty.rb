88642f4c7ac750693e5f711a2eadc3cdb33f2fa1
Brett Chalupa <brett@bigcartel.com>
2021-04-27 11:05:17 -0400

    Expand supported distance units, add initial weight units
    This makes it so that feet and inches are supported for converting
    distance. It also adds some initial weight units for converting between
    pounds and ounces--more units to be added in the future.
    
    This also ensures that the from and to units are able to be converted
    and of the same type.
    
    New errors are introduced to add clarity to invalid or incompatible unit
    types being passed in.

--- lib/converty.rb
+++ lib/converty.rb
@@ -6,33 +6,82 @@ require_relative "converty/version"
 module Converty
   class Error < StandardError; end
 
-  class CantConvertError < Error; end
+  # Error thrown when trying to convert between two different types of units,
+  # e.g. distance to weight.
+  class CantConvertError < Error
+    def initialize(from, to)
+      from_unit = UNITS.fetch(from)
+      to_unit = UNITS.fetch(to)
+      message = "can't convert #{from_unit.fetch(:type)} (#{from}) to #{to_unit.fetch(:type)} (#{to})"
+      super(message)
+    end
+  end
 
-  KILOMETERS_PER_MILE = 1.609344
+  # Thrown when a non-existant unit type is passed into the +from+ and/or +to+
+  # param when converting.
+  class UnitError < Error
+    def initialize(units)
+      message = units.map { |u| "#{u} is an invalid unit" }.join(", ")
+      super(message)
+    end
+  end
+
+  DISTANCE_TYPE = :distance
+  WEIGHT_TYPE = :weight
+
+  UNITS = {
+    ft: {
+      base_per: 1.0,
+      type: DISTANCE_TYPE
+    },
+    in: {
+      base_per: 1 / 12.0,
+      type: DISTANCE_TYPE
+    },
+    mi: {
+      base_per: 5280.0,
+      type: DISTANCE_TYPE
+    },
+    km: {
+      base_per: 3280.84,
+      type: DISTANCE_TYPE
+    },
+    oz: {
+      base_per: 1.0,
+      type: WEIGHT_TYPE
+    },
+    lb: {
+      base_per: 16.0,
+      type: WEIGHT_TYPE
+    }
+  }.freeze
 
   # Convert the specified amount in the +from+ unit to the to the +to+ unit
   #
   # It does not handle rounding the returned conversion.
   #
+  # Currently supports some distance and weight unit types.
+  #
   # Examples:
   #
   #  Converty.convert(5, from: :km, to: :mi).round(1) => 3.1
+  #  Converty.convert(16, from: :oz, to: :lb) => 1.0
   def self.convert(amount, from:, to:)
-    case from
-    when :mi
-      case to
-      when :km
-        amount * KILOMETERS_PER_MILE
-      else
-        raise CantConvertError
-      end
-    when :km
-      case to
-      when :mi
-        amount / KILOMETERS_PER_MILE
-      else
-        raise CantConvertError
-      end
+    to_unit = UNITS[to]
+    from_unit = UNITS[from]
+
+    if to_unit.nil? || from_unit.nil?
+      invalid_units = []
+      invalid_units.push(from) if from_unit.nil?
+      invalid_units.push(to) if to_unit.nil?
+      raise UnitError.new(invalid_units)
     end
+
+    if to_unit.fetch(:type) != from_unit.fetch(:type)
+      raise CantConvertError.new(from, to)
+    end
+
+    in_base = from_unit[:base_per] * amount
+    in_base / to_unit[:base_per]
   end
 end
--- spec/converty_spec.rb
+++ spec/converty_spec.rb
@@ -6,18 +6,65 @@ RSpec.describe Converty do
   end
 
   describe ".convert" do
-    it "supports miles to kilometers" do
-      expect(Converty.convert(3.1, from: :mi, to: :km).round(1)).to eql(5.0)
+    context "when converting distance" do
+      it "supports miles to kilometers" do
+        expect(Converty.convert(3.1, from: :mi, to: :km).round(1)).to eql(5.0)
+      end
+
+      it "supports kilometers to miles" do
+        expect(Converty.convert(5, from: :km, to: :mi).round(1)).to eql(3.1)
+      end
+
+      it "supports feet to inches" do
+        expect(Converty.convert(2, from: :ft, to: :in).round(1)).to eql(24.0)
+      end
+
+      it "supports miles to feet" do
+        expect(Converty.convert(1, from: :mi, to: :ft).round(1)).to eql(5280.0)
+        expect(Converty.convert(5, from: :mi, to: :ft).round(1)).to eql(26400.0)
+      end
+
+      it "supports feet to miles" do
+        expect(Converty.convert(6000, from: :ft, to: :mi).round(3)).to eql(1.136)
+      end
     end
 
-    it "supports kilometers to miles" do
-      expect(Converty.convert(5, from: :km, to: :mi).round(1)).to eql(3.1)
+    context "when converting weight" do
+      it "supports ounces to pounds" do
+        expect(Converty.convert(16, from: :oz, to: :lb).round(1)).to eql(1.0)
+
+        expect(Converty.convert(42, from: :oz, to: :lb)).to eql(2.625)
+      end
+
+      it "supports pounds to ounces" do
+        expect(Converty.convert(1, from: :lb, to: :oz)).to eql(16.0)
+
+        expect(Converty.convert(4.2, from: :lb, to: :oz)).to eql(67.2)
+      end
     end
 
-    it "throws an error when the units aren't compatible" do
-      expect { Converty.convert(5, from: :mi, to: :g) }.to raise_error(Converty::CantConvertError)
+    context "when there are errors" do
+      it "throws an error when a unit doesn't exist" do
+        expect do
+          Converty.convert(5, from: :not_real, to: :km)
+        end.to raise_error(Converty::UnitError, "not_real is an invalid unit")
+      end
 
-      expect { Converty.convert(5, from: :km, to: :g) }.to raise_error(Converty::CantConvertError)
+      it "throws an error when both units don't exist" do
+        expect do
+          Converty.convert(5, from: :not_real, to: :something_else)
+        end.to raise_error(Converty::UnitError, "not_real is an invalid unit, something_else is an invalid unit")
+      end
+
+      it "throws an error when the units aren't compatible" do
+        expect do
+          Converty.convert(5, from: :mi, to: :oz)
+        end.to raise_error(Converty::CantConvertError, "can't convert distance (mi) to weight (oz)")
+
+        expect do
+          Converty.convert(5, from: :lb, to: :km)
+        end.to raise_error(Converty::CantConvertError, "can't convert weight (lb) to distance (km)")
+      end
     end
   end
 end
